591e0ae2834a03dd186bf40f551dfe9c82734380
김동우 <sunbi5252@naver.com>
2022-07-29 00:08:35 +0900

    feat:회원 가입 추가

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -39,7 +39,7 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
-	@PostMapping()
+	@PostMapping("/signin")
 	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
     @ApiResponses({
         @ApiResponse(code = 200, message = "성공"),
@@ -49,9 +49,10 @@ public class UserController {
     })
 	public ResponseEntity<? extends BaseResponseBody> register(
 			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
-		
 		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
+		System.out.println(registerInfo.getPassword());
 		User user = userService.createUser(registerInfo);
+		System.out.println("hi");
 		
 		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
 	}
@@ -145,11 +146,6 @@ public class UserController {
 			return ResponseEntity.status(409).body(UserResponse.of(409, "중복된 아이디가 있습니다.", user));
 		}
 	
-		
-//		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
 	}
-	
-	
-	
 	
 }
