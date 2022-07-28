1a81ae45fa8b4c6965660ad9d1ff7c3d052820fa
김동우 <sunbi5252@naver.com>
2022-07-28 09:22:55 +0900

    병합 위한 push

--- backend/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -71,7 +71,7 @@ public class AuthController {
 	
 	
 	@PostMapping("/id-info")
-	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.") 
+	@ApiOperation(value = "아이디 중복 검사", notes = "<strong>아이디가 DB에 있는 지 확인한다.")
     @ApiResponses({
         @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
         @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
