b1e3586c00abefa0c5b392617d7753f8b1a558b4
김동우 <sunbi5252@naver.com>
2022-08-05 00:22:43 +0900

    feat: authority 세팅 추가

--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -11,6 +11,7 @@ import com.ssafy.db.entity.GameRecord;
 import com.ssafy.db.entity.User;
 import com.ssafy.db.repository.UserRepository;
 import com.ssafy.db.repository.UserRepositorySupport;
+import com.ssafy.infos.Authority;
 
 /**
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 구현 정의.
@@ -29,12 +30,14 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User createUser(UserRegisterPostReq userRegisterInfo) {
 		User user = new User();
+		
 		user.setId(userRegisterInfo.getId());
 		user.setName(userRegisterInfo.getName());
 		user.setNickname(userRegisterInfo.getNickname());
 		user.setPhonenumber(userRegisterInfo.getPhonenumber());
 		// 보안을 위해서 유저 패스워드 암호화 하여 디비에 저장.
 		user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
+		user.setAuthority(Authority.GENERAL.toString());
 		return userRepository.save(user);
 	}
 	
