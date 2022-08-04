58171f077a7038cbbd46814b79c1236d31edc1e5
김동우 <sunbi5252@naver.com>
2022-08-04 10:20:49 +0900

    login 추가

--- backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -100,32 +100,6 @@ public class UserController {
 //		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
 	}
 	
-	@GetMapping("/user-info")
-	@ApiOperation(value = "아이디 중복 검사", notes = "<strong>아이디가 DB에 있는 지 확인한다.")
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
-        @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-    })
-	public ResponseEntity<UserResponse> getUserInfo( @ApiParam(value="아이디 정보", required = true) @RequestParam("id")String id) {
-
-		User user = userService.getUserById(id);
-		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-		
-		if(user==null){
-			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-			System.out.println("해당 아이디에 맞는 user가 없습니다. 실패");
-			return ResponseEntity.ok(UserResponse.of(404, "failed", null));
-		}
-		else {
-			System.out.println("검색 성공");
-			return ResponseEntity.status(200).body(UserResponse.of(200, "중복된 아이디가 있습니다.", user));
-		}
-	
-		
-//		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
-	}
 	
 	@GetMapping("/nickname-info")
 	@ApiOperation(value = "아이디 중복 검사", notes = "<strong>아이디가 DB에 있는 지 확인한다.")
